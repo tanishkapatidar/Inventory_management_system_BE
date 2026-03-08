bab660d2ab67c3f6c53496f81f4cf12aab67a8d7
tanishkapatidar <tanishkapatidar18@gmail.com>
2026-03-09 01:20:28 +0530

    Fixed Type definition error by adding NoArgsConstructor to DTO

--- src/main/java/com/backend/inventory/dto/ProductRequestDTO.java
+++ src/main/java/com/backend/inventory/dto/ProductRequestDTO.java
@@ -3,14 +3,18 @@ package com.backend.inventory.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequestDTO {
 
     @NotBlank(message = "Name is required")
